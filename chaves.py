5fbf360bc2b614b48b9fa038383dacfd2a8df64f
GGOP5005 <gabsantos.04.05@gmail.com>
2026-03-30 11:18:28 -0300

    Ignora pasta de sessao temporaria e atualiza codigos

---
+++ chaves.py
@@ -0,0 +1,12 @@
+LISTA_CHAVES_GEMINI = [
+    "AIzaSyDs-hHWJGUWnYP-Fyv-agfN-tu0IXRa1OM", 
+    "AIzaSyCD9xDjNB3_mBKRKNKBWeNqRDY9f3h8SEw",                
+    "AIzaSyB-x5V0pU71a90kThs1uS4zKb4aWskNcbA",
+    "AIzaSyAbzmV8vvGW2pI2CrVSmAGoXsw5kLXm8iw",
+    "AIzaSyDBtEGf9xTw42sQmf_Vxdijg9AFJcWBhHs",
+    "AIzaSyA_eTw9fCl3xW_iI1sCIGCNBO28HVk-ciA",
+    "AIzaSyBx5u17G0aNUTwbnhF_kqXVFaL0TZQTWTg",
+    "AIzaSyCEE4mHmglMvZY50nGf3zQzbYOE2gve_Ac",
+    "AIzaSyCJx7E-9Tj72ku6PRvN_EG6S4VoqI24aYA",
+    "AIzaSyA4DAxMBuQnSbjlTwPFwVzDtt5ackbUxDg"
+]
